570cbd99b07015ed1c3693d298da5d0ca4ac6063
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 20:18:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,8 @@ def now_serving(katz_deli)
   if length == 0
     puts "There is nobody waiting to be served!"
   else
-    
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli = katz_deli.shift
+  end
 end
 
