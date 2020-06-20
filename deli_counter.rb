12f4e433e950d645735d1a53a9d5e7d6d6138b32
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 20:11:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+ 
+  line_length = katz_deli.length
+    if line_length == 0
+      puts "The line is currently empty."
+    else
+      puts "The line is currently: #{position}. #{name}"
+    end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  return "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+
